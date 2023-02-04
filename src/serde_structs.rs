859b4d3ded63a8d1d30bd13407887b73111f6012
Neil Morrison <email@neil.today>
2023-02-04 15:55:52 +0000

    Fully test the InstancePackager impl

--- src/serde_structs.rs
+++ src/serde_structs.rs
@@ -24,7 +24,7 @@ pub(crate) enum Sort {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub(crate) struct InstancePackagerOverrides {
-    pub(crate) data_slots: Vec<InstancePackagerDataSlot>,
+    pub(crate) data_slots: Option<Vec<InstancePackagerDataSlot>>,
     pub(crate) filename: Option<String>,
     // don't need to do memory_writes etc since those'll come in from a merge
     pub(crate) memory_writes: Option<Vec<SlotsCoresAndWrites>>,
@@ -44,8 +44,11 @@ pub(crate) struct InstancePackager {
 impl InstancePackager {
     pub fn get_slots(&self, folder_name: &str) -> Vec<InstancePackagerDataSlot> {
         if let Some(overides_map) = &self.overrides {
-            if let Some(overrides) = overides_map.get(folder_name) {
-                return overrides.data_slots.clone();
+            if let Some(data_slots) = overides_map
+                .get(folder_name)
+                .and_then(|o| o.data_slots.to_owned())
+            {
+                return data_slots.clone();
             }
         }
         self.data_slots.clone()
@@ -127,7 +130,7 @@ pub(crate) struct InstanceJSONInstance {
     pub memory_writes: Vec<SlotsCoresAndWrites>,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, PartialEq)]
 #[serde(untagged)]
 pub(crate) enum SlotsCoresAndWrites {
     CoreSelect { id: usize, select: bool },
@@ -152,8 +155,16 @@ impl InstanceJSON {
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        fs::{create_dir_all, File},
+        path::{Path, PathBuf},
+    };
+
+    use crate::serde_structs::SlotsCoresAndWrites;
+
     use super::InstancePackager;
     use serde_json::json;
+    use tempfile::tempdir;
 
     #[test]
     fn test_instance_packager_get_slots() {
@@ -191,5 +202,186 @@ mod tests {
         let data_slots = instance_packager.get_slots("non_overrider");
         assert_eq!(data_slots.len(), 1);
         assert_eq!(data_slots[0].filename, "file_A.bin");
+    }
+
+    #[test]
+    fn test_instance_packager_get_memory_writes() {
+        let json_data = json!({
+            "output": "some/folder/somewhere/",
+            "platform_id": "abc",
+            "data_slots": [
+                {
+                    "id": 22,
+                    "filename": "file_A.bin",
+                    "sort": "single",
+                    "required": true
+                }
+            ],
+            "memory_writes": [
+                {
+                    "data": "0x123",
+                    "address": "0x1345",
+                }
+            ],
+            "overrides": {
+                "overrider": {
+                    "memory_writes": [
+                        {
+                            "data": "0x9876",
+                            "address": "0x987654",
+                        },
+                        {
+                            "data": "0x9876",
+                            "address": "0x987654",
+                        }
+                    ]
+                }
+            }
+        });
+
+        let instance_packager: InstancePackager = serde_json::from_value(json_data).unwrap();
+
+        let expected = SlotsCoresAndWrites::MemoryWriteStr {
+            data: "0x123".to_string(),
+            address: "0x1345".to_string(),
+        };
+
+        let memory_writes = instance_packager.get_memory_writes("non_overrider");
+        assert_eq!(memory_writes.len(), 1);
+        assert_eq!(memory_writes[0], expected);
+
+        let expected = SlotsCoresAndWrites::MemoryWriteStr {
+            data: "0x9876".to_string(),
+            address: "0x987654".to_string(),
+        };
+
+        let memory_writes = instance_packager.get_memory_writes("overrider");
+        assert_eq!(memory_writes.len(), 2);
+        assert_eq!(memory_writes[0], expected);
+    }
+
+    #[test]
+    fn test_instance_packager_get_core_select() {
+        let json_data = json!({
+            "output": "some/folder/somewhere/",
+            "platform_id": "abc",
+            "core_select": {
+                "id": 123,
+                "select": true
+            },
+            "data_slots": [
+            ],
+            "overrides": {
+                "overrider": {
+                    "core_select": {
+                        "id": 456,
+                        "select": false
+                    }
+                }
+            }
+        });
+
+        let instance_packager: InstancePackager = serde_json::from_value(json_data).unwrap();
+
+        let expected = SlotsCoresAndWrites::CoreSelect {
+            id: 123,
+            select: true,
+        };
+
+        let core_select = instance_packager.get_core_select("non_overrider");
+        assert_eq!(core_select, Some(expected));
+
+        let expected = SlotsCoresAndWrites::CoreSelect {
+            id: 456,
+            select: false,
+        };
+
+        let core_select = instance_packager.get_core_select("overrider");
+        assert_eq!(core_select, Some(expected));
+    }
+
+    #[test]
+    fn test_instance_packager_get_file_name() {
+        let json_data = json!({
+            "output": "some/folder/somewhere/",
+            "platform_id": "abc",
+            "core_select": {
+                "id": 123,
+                "select": true
+            },
+            "data_slots": [
+            ],
+            "overrides": {
+                "overrider": {
+                    "filename": "overridden_file_name"
+                }
+            }
+        });
+
+        let instance_packager: InstancePackager = serde_json::from_value(json_data).unwrap();
+
+        let file_name = instance_packager
+            .get_filename(&PathBuf::from("fake/folder/game_name"))
+            .unwrap();
+        assert_eq!(file_name, String::from("game_name"));
+
+        let file_name = instance_packager
+            .get_filename(&PathBuf::from("fake/folder/overridden_file_name"))
+            .unwrap();
+        assert_eq!(file_name, String::from("overridden_file_name"));
+    }
+
+    #[test]
+    fn test_instance_packager_get_file_name_as_file_name() {
+        let json_data = json!({
+            "output": "some/folder/somewhere/",
+            "platform_id": "abc",
+            "core_select": {
+                "id": 123,
+                "select": true
+            },
+            "data_slots": [
+                {
+                    "id": 123,
+                    "filename": "*.cue",
+                    "as_filename": true,
+                    "sort": "single",
+                    "required": true
+                }
+            ],
+            "overrides": {
+                "overrider": {
+                    "filename": "overridden_file_name"
+                }
+            }
+        });
+
+        // TODO use the one in the `test_helpers` for this (need to move it somewhere)
+
+        let temp_dir = tempdir().unwrap();
+        let path = temp_dir.path();
+
+        let files = vec![
+            "fake/folder/game_name/cue_file_name.cue",
+            "fake/folder/overrider/cue_file_name.cue",
+        ];
+
+        for file in files {
+            let full_path = path.join(file);
+            create_dir_all(&full_path.parent().unwrap()).unwrap();
+            File::create(full_path).unwrap();
+        }
+
+        let instance_packager: InstancePackager = serde_json::from_value(json_data).unwrap();
+
+        let file_name = instance_packager
+            .get_filename(&path.join("fake/folder/game_name"))
+            .unwrap();
+        assert_eq!(file_name, String::from("cue_file_name"));
+
+        let file_name = instance_packager
+            .get_filename(&path.join("fake/folder/overrider"))
+            .unwrap();
+        assert_eq!(file_name, String::from("overridden_file_name"));
     }
 }
